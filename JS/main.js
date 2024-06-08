19e0abf71e83ed0d907d5b8a16a5cc21f54e5f3c
MuthuSelvam18 <muthuselvamjms18@gmail.com>
2024-06-08 14:28:17 +0530

    first

---
+++ JS/main.js
@@ -0,0 +1,9 @@
+var typed=new Typed(".text",{
+    strings:["Web Development","Software Development","Backend Development","Frontend Development"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
+
+          
